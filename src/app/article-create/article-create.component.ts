1b6127c93bd2f4ff1b9cb17ea8767be7d4b11fc1
Mehmet Emin Karagöz <karagozfamily1@gmail.com>
2022-08-23 23:43:45 +0200

    Article Create Form Validations has been implemented.
    When form submitted, validations control the form whether required field has been filled.

--- src/app/article-create/article-create.component.ts
+++ src/app/article-create/article-create.component.ts
@@ -1,6 +1,8 @@
+import { DeclarationListEmitMode } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Category } from '../models/category';
+import { AlertifyService } from '../services/alertify.service';
 import { ArticleService } from '../services/article.service';
 import { CategoryService } from '../services/category.service';
 
@@ -16,7 +18,8 @@ export class ArticleCreateComponent implements OnInit {
 
   constructor(private categoryService:CategoryService,
               private articleService:ArticleService,
-              private router:Router) { }
+              private router:Router,
+              private alertify:AlertifyService ) { }
 
   ngOnInit(): void {
     this.categoryService.getCategories().subscribe(data=>{
@@ -25,10 +28,37 @@ export class ArticleCreateComponent implements OnInit {
   }
 
   createArticle(title:any,description:any,imageUrl:any,categoryId:any){
-    console.log(title.value);
-    console.log(description.value);
-    console.log(imageUrl.value);
-    console.log(categoryId.value);
+
+    if(title.value===""|| description.value===""||imageUrl.value===""||categoryId.value==="-1"){
+      this.alertify.error("All information is required!");
+      return;
+    }
+
+    // extar controls and validations for article create form.
+
+    if(title.value.length<2){
+      this.alertify.error("Title must have min. 2 character")
+      return;
+    }
+    if(description.value.length<10){
+      this.alertify.error("Description must have min. 10 character")
+      return;
+    }
+
+    const extensions=["jpeg","jpg","png"];
+    const extension=imageUrl.value.split(".").pop();
+
+    if(extensions.indexOf(extension)===-1){
+      this.alertify.error("Only jpeg, jpg, png can be load");
+      return;
+    }
+
+
+
+
+
+
+
     
     const article={
     title:title.value,
